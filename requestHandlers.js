3c8e23f7d3be7ffb6bd409af8b97934678f2c93b
visidevi <32283668+visidevi@users.noreply.github.com>
2018-04-11 17:47:43 -0300

    enviar texto de una ruta a otra

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,17 +1,32 @@
-function iniciar() {
-    console.log("Manipulador de petición 'iniciar' ha sido llamado.");
-    function sleep(milliSeconds){
-        //obten la Hora actual
-        var starTime = new Date().getTime();
-        //atasca , la cpu
-        while (new Date().getTime() < starTime.milliSeconds);
-    } sleep(10000)
-    return 'Hola inciar'
-  }
+var exec = require("child_process").exec;
+var querystring = require('querystring')
 
-  function subir() {
-    console.log("Manipulador de petición 'subir' ha sido llamado.");
-    return 'hola Subir'
-  }
-  exports.iniciar = iniciar;
-  exports.subir = subir;
+function iniciar(response) {
+  console.log("Manipulador de petición 'iniciar' fue llamado.");
+  var body = `<html>
+    <head>
+    <meta http-equiv="Content-Type" content="text/html;
+        charset=UTF-8" />
+    </head>
+    <body>
+   <form action="/subir" method="post">
+   <textarea name="text" rows="20" cols="60"></textarea>
+   <input type="submit" value="Enviar texto" />
+   </form>
+   </body>
+   </html>`
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
+}
+
+function subir(response, dataPosteada) {
+  console.log("Manipulador de petición 'subir' fue llamado.");
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write(`Tu enviaste el texto ${querystring.parse(dataPosteada)['text']}`);
+  response.end();
+}
+
+exports.iniciar = iniciar;
+exports.subir = subir;
--- router.js
+++ router.js
@@ -1,10 +1,12 @@
-function route(handle , pathname){
+function route(handle, pathname, response, postData) {
     console.log('A punto de rutear una peticion para' + pathname)
-    if( typeof handle[pathname] === 'function'){
-        return handle[pathname]();
+    if (typeof handle[pathname] === 'function') {
+        return handle[pathname](response, postData);
     } else {
         console.log('No se encontro manipulador para' + pathname)
-        return '404 No encontrado'
+        response.writeHead(404, { 'Content-Type': 'text/html' });
+        response.write('404 No encontrado');
+        response.end();
     }
 }
 exports.route = route
